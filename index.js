4b9d411f77f757e7183e6441bdfc5a7e3be51efa
fatuma ismail <timaismail90@gmail.com>
2019-02-10 17:28:19 +0000

    recursive function added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN && toN === 0) {
+    return 0;
+  } else {
+    return fromN + sum(--toN)
+  }
 }
 
 module.exports = sum;
